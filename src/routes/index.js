05d7150fb7bfefc5b278142783aec9d1850c8c0b
fuhao <465476662@qq.com>
2016-11-10 16:15:32 +0800

    change routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import { Route } from 'react-router';
+import {
+	Route
+} from 'react-router';
 import rootNode from './rootNode';
 import * as Search from 'src/Search';
 import * as User from 'src/User';
 
-let routes = (
-    <div>
-        <Route path='/' component={rootNode}>
-            <Route path='search' component={Search.containers} />
-            <Route path='user/:name' component={User.containers} />
-        </Route>
-    </div>
-);
+// const routes = (
+//     <div>
+//         <Route path='/' component={rootNode}>
+//             <Route path='search' component={Search.containers} />
+//             <Route path='user/:name' component={User.containers} />
+//         </Route>
+//     </div>
+// );
+
+const routes = {
+	path: '/',
+	component: rootNode,
+	indexRoute: { onEnter: (nextState, replace) => replace('search') },
+	childRoutes: [{
+		path: 'search',
+		component: Search.containers
+	}, {
+		path: 'user/:name',
+		component: User.containers
+	}, ]
+};
 
 export default routes;
