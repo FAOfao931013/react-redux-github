36f5c7e1849300e5f9b49ed5bbe1b74a130b9d5d
fuhao <465476662@qq.com>
2016-12-27 14:10:58 +0800

    按需加载

--- webpack/dev.js
+++ webpack/dev.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
-var WebpackConfig = require('webpack-config');
-var path = require('path');
-var hotMiddlewareScript = 'webpack-hot-middleware/client?reload=true';
+const WebpackConfig = require('webpack-config');
+const path = require('path');
+const hotMiddlewareScript = 'webpack-hot-middleware/client?reload=true';
 
 module.exports = new WebpackConfig.Config().extend('./webpack/config-maker.js').merge({
     plugins: [
