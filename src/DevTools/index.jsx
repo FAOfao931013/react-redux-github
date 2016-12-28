d4b75de9afb768814c2193301449caafa2275d14
fuhao <465476662@qq.com>
2016-12-28 11:08:37 +0800

    去除无用代码

--- src/DevTools/index.jsx
+++ src/DevTools/index.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
-
 import { createDevTools } from 'redux-devtools';
-
 import DockMonitor from 'redux-devtools-dock-monitor';
 import LogMonitor from 'redux-devtools-log-monitor';
-//另一种Log
-// import FilterableLogMonitor from 'redux-devtools-filterable-log-monitor';
-// import ChartMonitor from 'redux-devtools-chart-monitor';
-// import SliderMonitor from 'redux-slider-monitor';
-// import DiffMonitor from 'redux-devtools-diff-monitor';
 
 const DevTools = createDevTools(
     <DockMonitor
