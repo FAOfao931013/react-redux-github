3ec8d16594cb42c06818f5cda01c0c8ce158c762
fuhao <465476662@qq.com>
2017-04-25 20:13:38 +0800

    更改节点

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -3,8 +3,12 @@ import { routerReducer } from 'react-router-redux';
 import * as Search from 'src/Search';
 import * as User from 'src/User';
 
-export default combineReducers({
-    [Search.constants.NAME]: Search.reducer,
+const componentsReducer = combineReducers({
+	[Search.constants.NAME]: Search.reducer,
     [User.constants.NAME]: User.reducer,
+});
+
+export default combineReducers({
+	components: componentsReducer,
     routing: routerReducer
 });
